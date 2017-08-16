6bb79b0d5005eff06be537b99e73e011777b0124
TarsilaaSouza <tarsila.santos@ccc.ufcg.edu.br>
2017-08-16 18:23:04 -0300

    Criacao da Classe HospitalAdapter

---
+++ src/main/java/com/ufcg/si1/model/HospitalAdapter.java
@@ -0,0 +1,37 @@
+package com.ufcg.si1.model;
+
+import br.edu.ufcg.Hospital;
+
+public class HospitalAdapter extends UnidadeSaude {
+
+	private Hospital hospital;
+	
+	public HospitalAdapter(String descricao, int medicos, int num) {
+		super(descricao);
+		this.hospital = new Hospital(descricao, medicos, num);
+	}
+	
+	public int getCodigo(){
+		return this.hospital.getCodigo();
+	}
+	
+	public void setCodigo (int codigo){
+		this.hospital.setCodigo(codigo);
+	}
+	
+	public String getDescricao(){
+		return this.hospital.getDescricao();
+	}
+	
+	public int getContador(){
+		return this.hospital.getContador();
+	}
+	
+	public int getNumeroMedicos(){
+		return this.hospital.getNumeroMedicos();
+	}
+	
+	public double getNumeroPacientePorDia(){
+		return this.hospital.getNumeroPacientesDia();
+	}
+}
